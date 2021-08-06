05c6dc6a9ccd2cd33cb924c84770c7f5c2218327
jmanke <jeffman879@gmail.com>
2021-08-05 19:55:31 -0700

    update news api source

--- news-api.js
+++ news-api.js
@@ -2,13 +2,13 @@
 const axios = require('axios');
 require('dotenv').config();
 
-const BASE_URL = 'https://newsdata.io/api/1/news';
+const BASE_URL = 'https://newsapi.org/v2';
 const API_KEY = process.env.NEWS_API_KEY;
 
 // Be sure to call authorize() before making requests
 class NewsApi {
   async getTopNews({ language = 'en', newsSource = 'cnn' }) {
-    const url = `${BASE_URL}?apikey=${API_KEY}&language=${language}&category=top&domain=${newsSource}`;
+    const url = `${BASE_URL}/top-headlines?apikey=${API_KEY}&language=${language}&sources=${newsSource}`;
     const res = await axios.get(url);
 
     return res.data;
