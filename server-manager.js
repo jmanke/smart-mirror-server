fef076ba5aea5a1138d442027459dd5884c8baf9
Jeff Manke <jeffman879@gmail.com>
2023-09-17 17:03:05 -0700

    better vercel domain

--- server-manager.js
+++ server-manager.js
@@ -104,6 +104,6 @@ let monitor = startServer();
 monitor.on('start', () => {
   autoUpdater();
   exec(
-    'unclutter -idle 0.1 & chromium-browser --incognito --kiosk https://smart-mirror-web-2-pyyvphfnw-hellodash.vercel.app/'
+    'unclutter -idle 0.1 & chromium-browser --incognito --kiosk https://smart-mirror-web-2.vercel.app/'
   );
 });
